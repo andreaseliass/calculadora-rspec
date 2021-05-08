a0d2109d2615258e53859a58e9e19811c3eb6617
Andrea Sampaio Elias <andreaase@hotmail.com>
2021-05-08 20:14:38 -0300

    update

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,5 +1,11 @@
 class Calculator
   def sum(a,b)
-    a+b
+    if a<0 or b<0
+      puts "Todos os valores devem ser maiores que zero."
+    end
+
+    if a>0 and b>0
+      a+b
+    end
   end
 end
--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -4,7 +4,22 @@ describe Calculator do
   it 'use sum method for 2 numbers' do
     calc = Calculator.new
     result = calc.sum(5,5)
-    expect(result).to eq(12)
+    expect(result).to eq(10)
   end
 
+  it 'sem negativos' do
+    calc = Calculator.new
+    result = calc.sum(5,-5)
+    is_expected.not_to be_an(Numeric)
+  end
+
+  it 'calibragem' do
+    calc = Calculator.new
+    result = calc.sum(5,5)
+    expect(result).to eq(10), "A Calculadora não está calibrada"
+  end
+
+
+
 end
+
