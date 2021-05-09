54a20d422aeced62f0b20e301a2dd076e88d711e
Andrea Sampaio Elias <andreaase@hotmail.com>
2021-05-09 14:52:26 -0300

    terceiro

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -1,25 +1,23 @@
 require 'calculator'
 
-describe Calculator do
-  it 'use sum method for 2 numbers' do
-    calc = Calculator.new
-    result = calc.sum(5,5)
-    expect(result).to eq(10)
+describe "Classe Calculadora" do
+
+  subject(:calc) { Calculator.new }
+  context "soma" do
+    it 'use sum method for 2 numbers' do
+      result = calc.sum(5,5)
+      expect(result).to eq(10)
+    end
+
+    it 'sem negativos' do
+      result = calc.sum(5,-5)
+      is_expected.not_to be_an(Numeric)
+    end
+
+    it 'calibragem' do
+      result = calc.sum(5,5)
+      expect(result).to eq(10), "A Calculadora não está calibrada"
+    end
   end
-
-  it 'sem negativos' do
-    calc = Calculator.new
-    result = calc.sum(5,-5)
-    is_expected.not_to be_an(Numeric)
-  end
-
-  it 'calibragem' do
-    calc = Calculator.new
-    result = calc.sum(5,5)
-    expect(result).to eq(10), "A Calculadora não está calibrada"
-  end
-
-
-
 end
 
