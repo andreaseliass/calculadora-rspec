4ae52046ca7820df74e0a32e8695162768cbb16f
Andrea Sampaio Elias <andreaase@hotmail.com>
2021-05-10 22:28:09 -0300

    alteracoes 10/05

--- spec/matchers/classes/classes_spec.rb
+++ spec/matchers/classes/classes_spec.rb
@@ -1,9 +1,6 @@
 require 'string_nao_vazia'
 
 describe 'Classes' do
-  it 'be_instance_of' do
-    expect(10).to be_instance_of(Integer) # Exatamente a classe
-  end
 
   it 'be_kind_of' do
     str = StringNaoVazia.new
